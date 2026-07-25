9ec4b78361456d9ea4b62b809bd12a25c31b240d
namelocsucram <marcuscoleman@live.com>
2026-07-25 14:26:25 -0500

    Initial Commit

---
+++ blocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blocks
+//
+//  Created by Owner on 7/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blocks/blocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  blocksApp.swift
+//  blocks
+//
+//  Created by Owner on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct blocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
